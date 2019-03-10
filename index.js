984c84e8fc9086ec980080183d11c5bb017c85ce
Tammy Metz <tammy@tammymetz.com>
2019-03-10 17:53:02 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function printBadges(empNames) {
+  for (let i = 0; i < empNames.length; i++) {
+    console.log(`Welcome ${empNames[i]}! You are employee #${i+1}.`);
+  }
+  return empNames;
+}
+
+function tailsNeverFails() {
+  let tails = 0;
+  let heads = 0;
+  while(heads === 0) {
+    if (Math.random() >= 0.5) {
+      tails++;
+    }
+    else {
+      heads++;
+    }
+  }
+  return (`You got ${tails} tails in a row!`);
+}
