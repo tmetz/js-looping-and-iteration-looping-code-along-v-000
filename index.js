27aa7d77c405582cbf409d20ea81af3f8b570a96
Tammy Metz <tammy@tammymetz.com>
2019-03-10 17:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Code your solutions in this file
+function printBadges(empNames) {
+  for (int i = 0; i < empNames.length; i++) {
+    console.log("Welcome ", empNames[i], "!  You are employee #", i, ".");
+  }
+}
